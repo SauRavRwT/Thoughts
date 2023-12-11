8ab17a0625bc506fb528202219f48172bd7954ed
S A U R A V <90666710+SauRavRwT@users.noreply.github.com>
2023-12-11 20:51:35 +0530

    Thoughts: added pagination and more thoughts...

--- README.md
+++ README.md
@@ -7,10 +7,30 @@ To generate a **random quote** from a text file, you can use a random number gen
      for (let i = 0; i < 9; i++) {
       displayRandomQuote();
      }
+*Pagination -*
 
-*Limit the displayed quote to 1000 characters -*
-    
-    quote.length > 1000
+    let currentPage = 1;
+    let pageSize = 15; // Adjust the number of quotes per page
+    let quotes = [];
+
+*Function to fetch quotes from the text file for a specific page -*
+
+    async function fetchQuotesForPage(page) {
+        const response = await fetch("./assets/Thoughts.txt");
+        const text = await response.text();
+        const allQuotes = text.split("\n").filter((quote) => quote.trim() !== "");
+        const startIndex = (page - 1) * pageSize;
+        const endIndex = startIndex + pageSize;
+        return allQuotes.slice(startIndex, endIndex);
+    }
+
+*Function to load the next page of quotes -*
+
+    async function loadNextPage() {
+        currentPage++;
+        document.getElementById("quoteGrid").innerHTML = ""; // Clear existing quotes
+        await displayQuotesForPage();
+    }
 
 *Auto Dark mode (CSS) -*
 
--- js/index.js
+++ js/index.js
@@ -1,40 +1,43 @@
-// Function to fetch quotes from the text file
-async function fetchQuotes() {
-  const response = await fetch("./assets/Thoughts.txt");
-  const text = await response.text();
-  return text.split("\n").filter((quote) => quote.trim() !== "");
+let currentPage = 1;
+let pageSize = 15; // Adjust the number of quotes per page
+let quotes = [];
+
+// Function to fetch quotes from the text file for a specific page
+async function fetchQuotesForPage(page) {
+    const response = await fetch("./assets/Thoughts.txt");
+    const text = await response.text();
+    const allQuotes = text.split("\n").filter((quote) => quote.trim() !== "");
+    const startIndex = (page - 1) * pageSize;
+    const endIndex = startIndex + pageSize;
+    return allQuotes.slice(startIndex, endIndex);
 }
 
-// Function to display a random quote
-async function displayRandomQuote() {
-  const quotes = await fetchQuotes();
-  const randomIndex = Math.floor(Math.random() * quotes.length);
-  const quote = quotes[randomIndex];
+// Function to display quotes for the current page
+async function displayQuotesForPage() {
+    const quotesForPage = await fetchQuotesForPage(currentPage);
 
-  // Limit the displayed quote to 1000 characters
-  const truncatedQuote =
-    quote.length > 1000 ? quote.slice(0, 1000) + "..." : quote;
-
-  const quoteElement = document.createElement("div");
-  quoteElement.classList.add("col-md-4", "mb-4");
-  quoteElement.innerHTML = `
-        <div class="card rounded-2">
-            <div class="card-body">
-                <p class="card-text">${truncatedQuote}</p>
+    quotesForPage.forEach(quote => {
+        const quoteElement = document.createElement("div");
+        quoteElement.classList.add("col-md-4", "mb-4");
+        quoteElement.innerHTML = `
+            <div class="card rounded-2">
+                <div class="card-body">
+                    <p class="card-text">${quote}</p>
+                </div>
             </div>
-        </div>
-    `;
+        `;
 
-  // Add the quote to the grid
-  document.getElementById("quoteGrid").appendChild(quoteElement);
+        // Add the quote to the grid
+        document.getElementById("quoteGrid").appendChild(quoteElement);
+    });
 }
 
-// Function to show the full quote
-// function showFullQuote(link, fullQuote) {
-//   link.parentNode.innerHTML = `<p class="card-text">${fullQuote}</p>`;
-// }
-
-// Display initial quotes
-for (let i = 0; i < 18; i++) {
-  displayRandomQuote();
+// Function to load the next page of quotes
+async function loadNextPage() {
+    currentPage++;
+    document.getElementById("quoteGrid").innerHTML = ""; // Clear existing quotes
+    await displayQuotesForPage();
 }
+
+// Initial load
+displayQuotesForPage();
